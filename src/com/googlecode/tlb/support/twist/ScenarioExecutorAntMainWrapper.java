08e9714a09a7bbf6a8280de07f6ade2cbda184d4
zee.ho.81 <zee.ho.81@f5287fb4-a01a-11dd-9a14-c1645b63b0c6>
2008-12-14 09:26:28 +0000

    the debug message is currently very useful for us. checkin this change for the debuging. we can remove it before we release.
    git-svn-id: https://test-load-balancer.googlecode.com/svn/trunk@92 f5287fb4-a01a-11dd-9a14-c1645b63b0c6

--- src/com/googlecode/tlb/support/twist/ScenarioExecutorAntMainWrapper.java
+++ src/com/googlecode/tlb/support/twist/ScenarioExecutorAntMainWrapper.java
@@ -1,18 +1,17 @@
 package com.googlecode.tlb.support.twist;
 
-import com.thoughtworks.twist.core.execution.ant.ScenarioExecutorAntMain;
-import com.googlecode.tlb.utils.FileUtil;
-import com.googlecode.tlb.support.junit.FilterFileSet;
 import com.googlecode.tlb.domain.GroupLoader;
 import com.googlecode.tlb.domain.GroupLoaderFactory;
+import com.googlecode.tlb.support.junit.FilterFileSet;
+import com.googlecode.tlb.utils.FileUtil;
+import com.thoughtworks.twist.core.execution.ant.ScenarioExecutorAntMain;
+import org.apache.tools.ant.DirectoryScanner;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.types.resources.FileResource;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
-
-import org.apache.tools.ant.DirectoryScanner;
-import org.apache.tools.ant.Project;
-import org.apache.tools.ant.types.resources.FileResource;
 
 public class ScenarioExecutorAntMainWrapper {
     public static final int PASS_EXIT_CODE = 0;
@@ -22,20 +21,24 @@ public class ScenarioExecutorAntMainWrapper {
     public static final int INVALID_INPUT_EXIT_CODE = 4;
 
     public static void main(String[] args) throws IOException {
+        System.out.println("Start executing twist scenarios.");
         File scenarioDir = new File(args[0]);
+        System.out.println("scenarios dir is [" + scenarioDir + "]");
         File reportsDir = new File(args[1]).getAbsoluteFile();
+        System.out.println("reports dir is [" + reportsDir + "]");
         String tags = "";
         if (args.length >= 2) {
             tags = args[2];
         }
-
+        System.out.println("tags are [" + tags + "]");
         File tempDir = FileUtil.createTempFolder();
 
         filter(scenarioDir, tempDir, GroupLoaderFactory.getInstance());
-
+        System.out.println("calling the original ant main");
         ScenarioExecutorAntMain.main(new String[]{tempDir.getAbsolutePath(),
                 reportsDir.getAbsolutePath(),
                 tags});
+        System.out.println("exited the original ant main");
     }
 
     static void filter(File scenarioDir, File tempFolder, GroupLoader groupLoader) throws IOException {
@@ -45,15 +48,15 @@ public class ScenarioExecutorAntMainWrapper {
         set.setDir(scenarioDir);
         set.setIncludes("**/*.scn");
         DirectoryScanner scanner = new DirectoryScanner();
-        
+
         set.setupDirectoryScanner(scanner, project);
         set.setProject(project);
-        
+
         Iterator iterator = set.iterator();
         while (iterator.hasNext()) {
             File src = ((FileResource) iterator.next()).getFile();
             File dest = new File(tempFolder, src.getName());
-            
+
             FileUtil.copySingleFile(src, dest);
         }
 
